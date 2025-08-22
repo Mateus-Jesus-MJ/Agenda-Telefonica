301222659ec45e85bc152ec7d1f6805442d535e8
Mateus-Jesus-MJ <157847342+Mateus-Jesus-MJ@users.noreply.github.com>
2025-08-21 21:45:07 -0300

    update, delete e buscar por nome na api

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -27,12 +27,52 @@ namespace ModuloApi.Controllers
         [HttpGet("{id}")]
         public IActionResult ObterPorId(int id)
         {
-            var contato = _context.Contato.Find(id);
+            var contato = _context.Contatos.Find(id);
 
             if (contato == null)
                 return NotFound();
 
             return Ok(contato);
+        }
+
+        [HttpGet("ObterPorNome")]
+        public IActionResult ObterPorId(nome id)
+        {
+            var contatos = _context.Contatos.Where(x => x.Nome.Contains(nome));
+            return Ok(contatos);
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, ContatoController contato)
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+
+            if (contato == null)
+                return NotFound();
+
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return Ok(contatoBanco);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+
+            if (contato == null)
+                return NotFound();
+
+            _context.Contatos.Remove(contatoBanco);
+            _context.SaveChanges();
+
+            return NoContent();
         }
     }
 }
