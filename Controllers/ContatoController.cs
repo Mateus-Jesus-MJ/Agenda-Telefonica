b849664648f3e2a1bee8db837ab7d1cad9b93bec
Mateus-Jesus-MJ <157847342+Mateus-Jesus-MJ@users.noreply.github.com>
2025-08-21 21:34:39 -0300

    metodo create e metodo obter por id

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using ModuloApi.Entities;
+using ModuloApi.Context;
 
 namespace ModuloApi.Controllers
 {
@@ -7,6 +9,30 @@ namespace ModuloApi.Controllers
     [Route("[controller]")]
     public class ContatoController : ControllerBase
     {
-        
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Contato contato)
+        {
+            _context.Add(contato);
+            _context.SaveChanges();
+            return OK(contato);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            var contato = _context.Contato.Find(id);
+
+            if (contato == null)
+                return NotFound();
+
+            return Ok(contato);
+        }
     }
 }
