36c679411257f5c7e3e7e8e600ae1c04ccaba2ee
Mateus-Jesus-MJ <157847342+Mateus-Jesus-MJ@users.noreply.github.com>
2025-08-21 21:26:34 -0300

    Commit inicial

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ModuloApi.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ContatoController : ControllerBase
+    {
+        
+    }
+}
